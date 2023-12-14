2ca156930b1078f863b2b8eb8fa76ce7799a7556
Luís Teixeira <luis.teixeira@arquiconsult.com>
2023-12-14 18:54:01 +0000

    App.xaml.cs missing code added

--- MauiLMTTemplate/App.xaml.cs
+++ MauiLMTTemplate/App.xaml.cs
@@ -2,11 +2,37 @@
 {
     public partial class App : Application
     {
-        public App()
+        private readonly INavigationService _navigationService;
+
+        public App(INavigationService navigationService)
         {
+            _navigationService = navigationService;
+
             InitializeComponent();
 
-            MainPage = new AppShell();
+            // OPTIONAL: Code that you need to run when Initing the App, some routine or Api call
+            InitApp(); 
+
+            MainPage = new AppShell(navigationService);
+        }
+
+        private void InitApp()
+        {
+            //OPTIONAL: Add code that you need to run when Initiating the App
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            //Add the code that should run when Starting the App
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+
+            //Add the code that should run when App is going into Sleep mode
         }
     }
 }
