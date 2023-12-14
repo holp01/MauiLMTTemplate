cc93f9383e777a025c7b83c6f9ca2965a3f8a56a
Luís Teixeira <luis.teixeira@arquiconsult.com>
2023-12-14 20:23:10 +0000

    Project with click on itself and passing Project as param.

---
+++ MauiLMTTemplate/ViewModels/ObservableCollectionViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace MauiLMTTemplate.ViewModels
+{
+    public class ObservableCollectionViewModel<T> : ObservableCollection<T>
+    {
+        public ObservableCollectionViewModel() : base()
+        {
+        }
+
+        public ObservableCollectionViewModel(IEnumerable<T> collection) : base(collection)
+        {
+        }
+
+        public ObservableCollectionViewModel(List<T> list) : base(list)
+        {
+        }
+
+        public void ReloadData(IEnumerable<T> items)
+        {
+            Items.Clear();
+
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+
+            OnPropertyChanged(new PropertyChangedEventArgs(nameof(Count)));
+            OnPropertyChanged(new PropertyChangedEventArgs("Items[]"));
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public async Task ReloadDataAsync(Func<IList<T>, Task> innerListAction)
+        {
+            Items.Clear();
+
+            await innerListAction(Items);
+
+            OnPropertyChanged(new PropertyChangedEventArgs(nameof(Count)));
+            OnPropertyChanged(new PropertyChangedEventArgs("Items[]"));
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
--- MauiLMTTemplate/ViewModels/ProjectViewModel.cs
+++ MauiLMTTemplate/ViewModels/ProjectViewModel.cs
@@ -1,17 +1,56 @@
-﻿using MauiLMTTemplate.ViewModels.Base;
+﻿using MauiLMTTemplate.Models.Projects;
+using MauiLMTTemplate.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiLMTTemplate.ViewModels
 {
-    public class ProjectViewModel : ViewModelBase
+    public partial class ProjectViewModel : ViewModelBase
     {
+        private readonly ObservableCollectionViewModel<Project> _projects = new();
+
+        public IReadOnlyList<Project> Projects => _projects;
+
         public ProjectViewModel(INavigationService navigationService)
             : base(navigationService)
         {
+
+        }
+
+        public override async Task InitializeAsync()
+        {
+            if (IsInitialized)
+                return;
+
+            IsInitialized = true;
+            await IsBusyFor(
+                async () =>
+                {
+                    //Simulate api call
+                    var projects = new List<Project>()
+                    {
+                        new Project { Id = 1, Name = "Project 1", Description = "This is the first project." },
+                        new Project { Id = 2, Name = "Project 2", Description = "This is the second project." },
+                        new Project { Id = 3, Name = "Project 3", Description = "This is the third project." }
+                    };
+
+                    _projects.ReloadData(projects);
+                });
+
+        }
+
+        [RelayCommand]
+        private async Task ProjectTappedAsync(Project project)
+        {
+            await IsBusyFor(
+                async () =>
+                {
+                    await Task.Delay(10);
+                });
 
         }
     }
